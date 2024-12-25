6f65f4e03fa016f8bbcc1d0c80cc503610a4e805
Nicolas Delhaye <nicolas.delhaye62220@gmail.com>
2024-12-26 00:23:40 +0100

    Mise en place du routeur Ajout d'un menu et d'un footer Utilisation de bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
 <template>
-  <h1>Bienvenue sur la première version du site de {{ name }}</h1>
-  <button v-on:click="addName">Ajouter</button>
-  <button v-on:click="delName">Retirer</button>
-  <ul>
-    <li v-for="item in list" v-bind:key="item">{{ item }}</li>
-  </ul>
+  <Menu />
+  <router-view></router-view>
+  <Footer />
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-let name = ref('Nicolas Delhaye')
-let list = ref([
-    "Nicolas Delhaye"
-])
-
-const addName = () => {
-    list.value.push('Nicolas Delhaye')
-}
-
-const delName = () => {
-    list.value.pop()
-}
+import Menu from './components/Menu.vue'
+import Footer from './components/footer.vue'
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Bienvenue sur la première version du site de {{ name }}</h1>
+    <button v-on:click="addName">Ajouter</button>
+    <button v-on:click="delName">Retirer</button>
+    <ul>
+      <li v-for="item in list" v-bind:key="item">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let name = ref('Nicolas Delhaye')
+let list = ref([
+  "Nicolas Delhaye"
+])
+
+const addName = () => {
+  list.value.push('Nicolas Delhaye')
+}
+
+const delName = () => {
+  list.value.pop()
+}
+</script>
